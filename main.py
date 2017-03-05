940843cb4c5372712b5bd4bdba488e6a583bc51a
marcmad5 <marcmad12345@gmail.com>
2017-03-05 16:09:49 -0500

    Ajout des phrases jusqu'à 9

--- main.py
+++ main.py
@@ -21,9 +21,21 @@ tokens0 = "Les et Les"
 tokens = "Le Jean tua Marie."
 tokens1 = "Les suspects sont Claude, Jeannette, Ivan, Karl et John."
 tokens2 = "La victime a été retrouvé dans le salon à 21 heures."
+tokens3 = "Le corps est chaud."
+tokens4 = "Claude a quitté le salon vers 19 heures."
+tokens5 = "Il mangeait un sac de chips."
+tokens6 = "C'est Karl qui a trouvé le corps à 23 heures."
+tokens7 = "Les objets trouvés sur la scène du crime sont un couteau de cuisine, un sac de chips et un verre d'alcool vide."
+#tokens8 = "Le couteau est taché de sang."
+#tokens8 = "Le couteau de cuisine est ensanglanté"
+tokens8 = "Le couteau de cuisine est ensanglanté."
+tokens9 = "Jeannette, Ivan et Karl n'ont pas bu."
+#tokens10 = "Claude et jeannette ont vu un homme dans le salon en compagnie de la victime vers 21 heure."
+tokens11 = "La victime n'a pas d’alcool dans son sang."
+tokens12 = "Claude est le propriétaire du couteau de cuisine, mais il l'a prêté à John."
 #Texte complet de MAD
-tokens3 = enonce.split()
-splittedText = nltk.word_tokenize(tokens1)
+#tokens3 = enonce.split()
+splittedText = nltk.word_tokenize(tokens8)
 
 parser = parse.FeatureEarleyChartParser(grammar)
 trees = parser.parse(splittedText)
