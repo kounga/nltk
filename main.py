0ad519d373599f081eecc17c89adb8a30535c8fc
Yassine Rachid <am45880@etsmtl.net>
2017-03-12 13:14:28 -0500

    Code Formatter , enlever quelques procedures

--- main.py
+++ main.py
@@ -3,50 +3,55 @@ import codecs
 from nltk import *
 from Lexical import LexiqueClass
 
-verbs = {
-    "suspects" : "is-suspect",
-    "dans" : "at-loc",
-    "victime" : "victim",
-    "trouvé" : "has-found",
-    "objects" : "object",
-    "sur" : "at-loc",
-    "pas" : "has-not"
-    }
+prepToVerbs = {
+    "suspects": "is-suspect",
+    "dans": "at-loc",
+    "victime": "victim",
+    "trouvé": "has-found",
+    "objects": "object",
+    "sur": "at-loc",
+    "pas": "has-not"
+}
 
-enableDebug=True
-allFacts=[]
+quitteFrancais = "quitté"
+salonFrancais = "salon"
+alcoolFrancais = "alcool"
+heuresFrancais = "heures"
+pronomFrancais = "Il"
+claudeFrancais = "Claude"
+sangFrancais = "sang"
+victimeFrancais = "victime"
+
+enableDebug = True
+allFacts = []
 
 with codecs.open("grammaire.cfg", 'r', encoding='utf8') as f:
-    grammaireText2=f.read()
+    grammaireText2 = f.read()
 
-#Lecture de l'énoncé dans le fichier txt
+# Lecture de l'énoncé dans le fichier txt
 with codecs.open("histoire.txt", 'r', encoding='utf8') as f:
     histoire = f.read()
 
-phrases_de_histoire = nltk.sent_tokenize(histoire,'french')
+phrases_de_histoire = nltk.sent_tokenize(histoire, 'french')
 
-i=0
+i = 0
 for phrase in phrases_de_histoire:
-    i+=1
-    print(i,")",phrase)
-
-
-
-
+    i += 1
+    print(i, ")", phrase)
 
 grammar = grammar.FeatureGrammar.fromstring(grammaireText2)
 parser = nltk.ChartParser(grammar)
 
 print("")
 print("=====================================================================")
-i=0
+i = 0
 for phrase_a_valider in phrases_de_histoire:
     try:
         splittedText = nltk.word_tokenize(phrase_a_valider)
         trees = parser.parse(splittedText)
     except:
-        print("« ",phrase_a_valider," » n'est pas syntaxiquement correct")
-    i+=1
+        print("« ", phrase_a_valider, " » n'est pas syntaxiquement correct")
+    i += 1
 
 trees = None
 splittedText = None
@@ -60,8 +65,8 @@ parser = parse.FeatureEarleyChartParser(grammar)
 print("")
 print("=====================================================================")
 print("Phrase 1 : {phrase}".format(phrase=phrases_de_histoire[0]))
-cleanedWords=[]
-facts=[]
+cleanedWords = []
+facts = []
 splittedText = nltk.word_tokenize(phrases_de_histoire[0])
 trees = parser.parse(splittedText)
 
@@ -83,23 +88,22 @@ else:
 
     # Clean up
     for word in rawWords:
-         cleanWord = word.replace("(","").replace(")","").strip()
-         cleanedWords.append(cleanWord)
+        cleanWord = word.replace("(", "").replace(")", "").strip()
+        cleanedWords.append(cleanWord)
 
     # Construct facts
     for word in cleanedWords:
-        if word not in verbs:
-            allFacts.append("({verb} {word})".format(verb=verbs[cleanedWords[0]], word=word))
+        if word not in prepToVerbs:
+            allFacts.append("({verb} {word})".format(verb=prepToVerbs[cleanedWords[0]], word=word))
             if enableDebug:
-                print("({verb} {word})".format(verb=verbs[cleanedWords[0]], word=word))
-
+                print("({verb} {word})".format(verb=prepToVerbs[cleanedWords[0]], word=word))
 
 # =====================================================================
 print("")
 print("=====================================================================")
 print("Phrase 2 : {phrase}".format(phrase=phrases_de_histoire[1]))
-cleanedWords=[]
-facts=[]
+cleanedWords = []
+facts = []
 splittedText = nltk.word_tokenize(phrases_de_histoire[1])
 trees = parser.parse(splittedText)
 
@@ -121,22 +125,22 @@ else:
 
     # Clean up
     for word in rawWords:
-         cleanWord = word.replace("(","").replace(")","").strip()
-         cleanedWords.append(cleanWord)
+        cleanWord = word.replace("(", "").replace(")", "").strip()
+        cleanedWords.append(cleanWord)
 
     nbItems = 0
     for i in cleanedWords:
-        nbItems+=1
+        nbItems += 1
 
     # Construct facts
-    fact=""
+    fact = ""
     for i in range(0, nbItems):
         if cleanedWords[i].lower() == "corps":
-            fact = "corps"
-        elif cleanedWords[i].lower() == "heures":
-            fact += " at " + cleanedWords[i-1]
+            fact = cleanedWords[i].lower()
+        elif cleanedWords[i].lower() == heuresFrancais:
+            fact += " at " + cleanedWords[i - 1]
         elif cleanedWords[i].lower() == "dans":
-            fact += " at-loc " + cleanedWords[i+1]
+            fact += " at-loc " + cleanedWords[i + 1]
 
     if enableDebug:
         print("({fact})".format(fact=fact))
@@ -146,8 +150,8 @@ else:
 print("")
 print("=====================================================================")
 print("Phrase 3 : {phrase}".format(phrase=phrases_de_histoire[2]))
-cleanedWords=[]
-facts=[]
+cleanedWords = []
+facts = []
 splittedText = nltk.word_tokenize(phrases_de_histoire[2])
 trees = parser.parse(splittedText)
 
@@ -169,12 +173,12 @@ else:
 
     # Clean up
     for word in rawWords:
-         cleanWord = word.replace("(","").replace(")","").strip()
-         cleanedWords.append(cleanWord)
+        cleanWord = word.replace("(", "").replace(")", "").strip()
+        cleanedWords.append(cleanWord)
 
-    fact=""
+    fact = ""
     for word in cleanedWords:
-        fact+=word + " "
+        fact += word + " "
 
     if enableDebug:
         print("(description {fact})".format(fact=fact))
@@ -183,9 +187,9 @@ else:
 # =====================================================================
 print("")
 print("=====================================================================")
-print("Phrase 4 : ",phrases_de_histoire[3])
-cleanedWords=[]
-facts=[]
+print("Phrase 4 : ", phrases_de_histoire[3])
+cleanedWords = []
+facts = []
 splittedText = nltk.word_tokenize(phrases_de_histoire[3])
 trees = parser.parse(splittedText)
 
@@ -193,7 +197,7 @@ for tree in trees:
     # uncomment to see the trees
     # nltk.draw.tree.draw_trees(tree)
     rawWords = str(tree.label()['SEM']).split(',')
-    
+
 try:
     if rawWords is None:
         print("rawWords is None!")
@@ -203,25 +207,25 @@ else:
     # Debug
     if enableDebug:
         print(rawWords)
-        
-    i=0
-    for element in rawWords:
-        rawWords[i]=element.replace("(","").replace(")","").replace(" ","")
-        i+=1
 
-    fact="("
-    i=0
+    i = 0
+    for element in rawWords:
+        rawWords[i] = element.replace("(", "").replace(")", "").replace(" ", "")
+        i += 1
+
+    fact = "("
+    i = 0
     for word in rawWords:
-        if word == "Claude":
-            fact += "Claude"
-        elif word == "quitté":
+        if word == claudeFrancais:
+            fact += word
+        elif word == quitteFrancais:
             fact += " exit "
-        elif  word == "salon":
-            fact += "salon"
-        elif  word == "heures":
-            fact += " at " + rawWords[i-1]
-        i+=1;
-    fact+=")"
+        elif word == salonFrancais:
+            fact += word
+        elif word == heuresFrancais:
+            fact += " at " + rawWords[i - 1]
+        i += 1;
+    fact += ")"
 
     if enableDebug:
         print(fact)
@@ -231,9 +235,9 @@ else:
 # =====================================================================
 print("")
 print("=====================================================================")
-print("Phrase 5 : ",phrases_de_histoire[4])
-cleanedWords=[]
-facts=[]
+print("Phrase 5 : ", phrases_de_histoire[4])
+cleanedWords = []
+facts = []
 splittedText = nltk.word_tokenize(phrases_de_histoire[4])
 trees = parser.parse(splittedText)
 
@@ -251,26 +255,25 @@ else:
     # Debug
     if enableDebug:
         print(rawWords)
-      
-    i=0
-    for element in rawWords:
-        rawWords[i]=element.replace("(","").replace(")","").replace(" ","")
-        i+=1
 
-    fact="("
-    i=0
+    i = 0
+    for element in rawWords:
+        rawWords[i] = element.replace("(", "").replace(")", "").replace(" ", "")
+        i += 1
+
+    fact = "("
+    i = 0
     for word in rawWords:
 
-        if word == "Il":
-            fact += "Claude"
+        if word == pronomFrancais:
+            fact += claudeFrancais
         elif word == "mangeait":
             fact += " ate "
-        elif  word == "sacdechips":
-            fact += "chips at 19"
+        elif word == "sacdechips":
+            fact += word
+        i += 1;
 
-        i+=1;
-
-    fact+=")"
+    fact += ")"
 
     if enableDebug:
         print(fact)
@@ -281,8 +284,8 @@ else:
 print("")
 print("=====================================================================")
 print("Phrase 6 : {phrase}".format(phrase=phrases_de_histoire[5]))
-cleanedWords=[]
-facts=[]
+cleanedWords = []
+facts = []
 splittedText = nltk.word_tokenize(phrases_de_histoire[5])
 trees = parser.parse(splittedText)
 
@@ -304,20 +307,20 @@ else:
 
     # Clean up
     for word in rawWords:
-         cleanWord = word.replace("(","").replace(")","").strip()
-         cleanedWords.append(cleanWord)
+        cleanWord = word.replace("(", "").replace(")", "").strip()
+        cleanedWords.append(cleanWord)
 
-         nbItems = 0
+        nbItems = 0
     for i in cleanedWords:
-        nbItems+=1
+        nbItems += 1
 
     # Construct facts
-    fact=""
+    fact = ""
     for i in range(0, nbItems):
         if cleanedWords[i].lower() == "trouvé":
-            fact = verbs[cleanedWords[i]] + " " + cleanedWords[i-1]
+            fact = prepToVerbs[cleanedWords[i]] + " " + cleanedWords[i - 1]
         elif cleanedWords[i].lower() == "heures":
-            fact += " at " + cleanedWords[i-1]
+            fact += " at " + cleanedWords[i - 1]
         elif not cleanedWords[i].isdigit():
             fact += " " + cleanedWords[i]
 
@@ -329,8 +332,8 @@ else:
 print("")
 print("=====================================================================")
 print("Phrase 7 : {phrase}".format(phrase=phrases_de_histoire[6]))
-cleanedWords=[]
-facts=[]
+cleanedWords = []
+facts = []
 splittedText = nltk.word_tokenize(phrases_de_histoire[6])
 trees = parser.parse(splittedText)
 
@@ -339,7 +342,7 @@ for tree in trees:
     # uncomment to see the trees
     # nltk.draw.tree.draw_trees(tree)
     rawWords = str(tree.label()['SEM']).split(',')
-    
+
 try:
     if rawWords is None:
         print("rawWords is None!")
@@ -352,27 +355,27 @@ else:
 
     # Clean up
     for word in rawWords:
-         cleanWord = word.replace("(","").replace(")","").strip()
-         cleanedWords.append(cleanWord)
+        cleanWord = word.replace("(", "").replace(")", "").strip()
+        cleanedWords.append(cleanWord)
 
-         nbItems = 0
+        nbItems = 0
     for i in cleanedWords:
-        nbItems+=1
+        nbItems += 1
 
     # Construct facts
-    fact=""
+    fact = ""
     curCount = 0
     for i in range(0, nbItems - 1):
-        if cleanedWords[i] in verbs:
-            if verbs[cleanedWords[i]]=="at-loc":
-                fact+=verbs[cleanedWords[i]] + " " + cleanedWords[i + 1]
+        if cleanedWords[i] in prepToVerbs:
+            if prepToVerbs[cleanedWords[i]] == "at-loc":
+                fact += prepToVerbs[cleanedWords[i]] + " " + cleanedWords[i + 1]
             else:
-                fact+=verbs[cleanedWords[i]] + " "
-            curCount+=1
+                fact += prepToVerbs[cleanedWords[i]] + " "
+            curCount += 1
 
-    curCount+=1
+    curCount += 1
     for i in range(curCount, nbItems - 1):
-        if cleanedWords[i] not in verbs:
+        if cleanedWords[i] not in prepToVerbs:
             if enableDebug:
                 print("({partial_fact} {obj} {desc})".format(partial_fact=fact, obj=cleanedWords[i], desc=""))
             allFacts.append("({partial_fact} {obj} {desc})".format(partial_fact=fact, obj=cleanedWords[i], desc=""))
@@ -381,8 +384,8 @@ else:
 print("")
 print("=====================================================================")
 print("Phrase 8 : {phrase}".format(phrase=phrases_de_histoire[7]))
-cleanedWords=[]
-facts=[]
+cleanedWords = []
+facts = []
 splittedText = nltk.word_tokenize(phrases_de_histoire[7])
 trees = parser.parse(splittedText)
 
@@ -404,20 +407,20 @@ else:
 
     # Clean up
     for word in rawWords:
-         cleanWord = word.replace("(","").replace(")","").strip()
-         cleanedWords.append(cleanWord)
+        cleanWord = word.replace("(", "").replace(")", "").strip()
+        cleanedWords.append(cleanWord)
 
-         nbItems = 0
+        nbItems = 0
     for i in cleanedWords:
-        nbItems+=1
+        nbItems += 1
 
     # Construct facts
-    fact=""
+    fact = ""
     curCount = 0
 
-    fact=""
+    fact = ""
     for word in cleanedWords:
-        fact+=word + " "
+        fact += word + " "
 
     if enableDebug:
         print("(description {fact})".format(fact=fact))
@@ -427,8 +430,8 @@ else:
 print("")
 print("=====================================================================")
 print("Phrase 9 : {phrase}".format(phrase=phrases_de_histoire[8]))
-cleanedWords=[]
-facts=[]
+cleanedWords = []
+facts = []
 splittedText = nltk.word_tokenize(phrases_de_histoire[8])
 trees = parser.parse(splittedText)
 
@@ -450,33 +453,31 @@ else:
 
     # Clean up
     for word in rawWords:
-         cleanWord = word.replace("(","").replace(")","").strip()
-         cleanedWords.append(cleanWord)
-
+        cleanWord = word.replace("(", "").replace(")", "").strip()
+        cleanedWords.append(cleanWord)
 
     nbItems = 0
     for i in cleanedWords:
-        nbItems+=1
+        nbItems += 1
 
     # Construct facts
-    fact=""
+    fact = ""
     curCount = 0
     for i in range(nbItems - 1, 0, -1):
-        if cleanedWords[i] in verbs:
-            fact=verbs[cleanedWords[i]] + " " + cleanedWords[i+1]
-            curCount+=1
+        if cleanedWords[i] in prepToVerbs:
+            fact = prepToVerbs[cleanedWords[i]] + " " + cleanedWords[i + 1]
+            curCount += 1
         elif cleanedWords[i] == "bu":
-            curCount+=1
+            curCount += 1
 
     for i in range(nbItems - 1 - curCount, -1, -1):
         if enableDebug:
             print("({partial_fact} {person}".format(partial_fact=fact, person=cleanedWords[i]))
         allFacts.append("({partial_fact} {person}".format(partial_fact=fact, person=cleanedWords[i]))
 
-
 print("")
 print("=====================================================================")
-print("Phrase 10 : ",phrases_de_histoire[9])
+print("Phrase 10 : ", phrases_de_histoire[9])
 cleanedWords = []
 facts = []
 splittedText = nltk.word_tokenize(phrases_de_histoire[9])
@@ -502,13 +503,13 @@ else:
         rawWords[i] = element.replace("(", "").replace(")", "").replace(" ", "")
         i += 1
 
-    fact = "( etat victime "
+    fact = "( etat  " + victimeFrancais + " "
     i = 0
     for word in rawWords:
-        if word == "sang":
-            fact += "sang"
-        elif word == "alcool" and rawWords[i-1]=="pas":
-            fact += "( not alcool "
+        if word == sangFrancais:
+            fact += word
+        elif word == alcoolFrancais and rawWords[i - 1] == "pas":
+            fact += "( not " + word + " "
         i += 1;
     fact += "))"
 
@@ -517,12 +518,11 @@ else:
 
     allFacts.append(fact)
 
-
 print("")
 print("=====================================================================")
 print("Phrase 11 : {phrase}".format(phrase=phrases_de_histoire[10]))
-cleanedWords=[]
-facts=[]
+cleanedWords = []
+facts = []
 splittedText = nltk.word_tokenize(phrases_de_histoire[10])
 trees = parser.parse(splittedText)
 
@@ -544,35 +544,34 @@ else:
 
     # Clean up
     for word in rawWords:
-         cleanWord = word.replace("(","").replace(")","").strip()
-         cleanedWords.append(cleanWord)
-         
-    proprietaire=""
-    locateur="John"
-    quoi=""
-    fact=""
-            
+        cleanWord = word.replace("(", "").replace(")", "").strip()
+        cleanedWords.append(cleanWord)
+
+    proprietaire = ""
+    locateur = "John"
+    quoi = ""
+    fact = ""
+
     nbItems = 0
     for i in cleanedWords:
-        nbItems+=1
-        
+        nbItems += 1
+
     for i in range(0, nbItems - 1):
         if cleanedWords[i] != "mais":
             if cleanedWords[i] == "couteau de cuisine":
                 quoi = cleanedWords[i]
             elif cleanedWords[i] == "Claude":
-                proprietaire=cleanedWords[i]
-                  
-    fact="({action} {what} {who})".format(what=quoi, action=cleanedWords[i], who=proprietaire)
+                proprietaire = cleanedWords[i]
+
+    fact = "({action} {what} {who})".format(what=quoi, action=cleanedWords[i], who=proprietaire)
     allFacts.append(fact)
-    fact="({action} {what} {who})".format(what=quoi, action="en-possession", who=locateur)
+    fact = "({action} {what} {who})".format(what=quoi, action="en-possession", who=locateur)
     allFacts.append(fact)
     print(fact)
-    
 
 fichierFact = codecs.open("facts.txt", "w", "utf-8")
-#Creation du fichier Facts.txt avec les faits Jess
+# Creation du fichier Facts.txt avec les faits Jess
 for fact in allFacts:
-    fact+="\n"
+    fact += "\n"
     fichierFact.writelines(fact)
 fichierFact.close()
